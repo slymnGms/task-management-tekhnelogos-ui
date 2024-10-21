be78139bb82fbed064197904637af2c7749eed41
Suleyman GUMUS <suleyman_gumus@outlook.com.tr>
2024-10-22 01:58:09 +0300

    app changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
+import { AuthGuard } from './guards/auth.guard';
+import { TaskCreateComponent } from './components/tasks/task-create/task-create.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MyTasksComponent } from './components/my-tasks/my-tasks.component';
+import { AssignTasksComponent } from './components/tasks/assign-tasks/assign-tasks.component';
+import { ManageAssignmentsComponent } from './components/tasks/manage-assignments/manage-assignments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Auth routes
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // Protected routes
+  { path : '', component : DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'my-tasks', component: MyTasksComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/list', component: TaskListComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/create', component: TaskCreateComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/assign', component: AssignTasksComponent, canActivate: [AuthGuard] },
+  { path: 'tasks/assignments', component: ManageAssignmentsComponent, canActivate: [AuthGuard] },
+
+  // Other routes
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { TaskListComponent } from './components/tasks/task-list/task-list.component';
+import { TaskCreateComponent } from './components/tasks/task-create/task-create.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { MenubarModule } from 'primeng/menubar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AssignTasksComponent } from './components/tasks/assign-tasks/assign-tasks.component';
+import { PickListModule } from 'primeng/picklist';
+import { ListboxModule } from 'primeng/listbox';
+import { MyTasksComponent } from './components/my-tasks/my-tasks.component';
+import { ChartModule } from 'primeng/chart';
+import { UserTaskGraphComponent } from './components/displays/user-task-graph/user-task-graph.component';
+import { ManageAssignmentsComponent } from './components/tasks/manage-assignments/manage-assignments.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TaskListComponent,
+    TaskCreateComponent,
+    DashboardComponent,
+    NavbarComponent,
+    AssignTasksComponent,
+    MyTasksComponent,
+    UserTaskGraphComponent,
+    ManageAssignmentsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    //#region PrimeNG
+    TableModule,
+    CalendarModule,
+    MenubarModule,
+    PickListModule,
+    ListboxModule,
+    ChartModule,
+    //#endregion
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
